5a0e4ab624fcebb84c1be46ddfec737470b83e82
Jonathan <jonaherrera90@hotmail.com>
2024-12-06 15:21:23 -0600

    Corrigiendo errores15

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
+
 // https://astro.build/config
 export default defineConfig({
   output:"server",
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: netlify()
 });
