d69f82566cabbf25cc53a0fb0cf169d59313bce7
Jonathan <jonaherrera90@hotmail.com>
2024-12-06 15:42:58 -0600

    Cambio de idioma

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -3,13 +3,48 @@ export const languages = {
     es: 'Español',
   };
   
-  export const defaultLang = 'en';
+  export const defaultLang = 'es';
   
   export const ui = {
     en: {
-      'saludo':'Hi, my name is Jonathan'
+      'saludo':'Hi, my name is Jonathan',
+      'nombre':'Jonathan Herrera',
+      'profecion':"Desarrollador Full Stack",
+      'descripcion':"Hola, soy Jonathan Herrera, desarrollador y diseñador web. Meapasiona crear sitios web hermosos, accesibles y de altorendimiento. También soy un gran fan de Astro, la forma más rápida de construir sitios web modernos.",
+      'trabajo':"Conoce un poco mas de mi trabajo",
+      'inicio':"Inicio",
+      'sobreMi':"Sobre mi",
+      'proyectos':"Proyectos",
+      'contacto':"Contacto",
+      'ProyectName1':'Proyecto de mascotas',
+      'ProyectName2':'RentRide',
+      'ProyectName3':'FitNation',
+      'proyectDescription1':'Desarrollé una plataforma de reservas utilizando HTML, CSS, JavaScript y bases de datos SQL, diseñada para conectar a usuarios que buscaban servicios para sus mascotas con proveedores que ofrecían dichos servicios. La aplicación permitía gestionar las reservas de manera eficiente y mejoraba la experiencia tanto para clientes como para proveedores.',
+      'proyectDescription2':'Desarrollé una plataforma de alquiler de vehículos con .NET y Azure, implementando gestión de reservas y usuarios, y usando SQL para almacenar información. Trabajamos bajo SCRUM, superando desafíos de integración en la nube para entregar una solución escalable.',
+      'proyectDescription3':'Desarrollé una página de reserva de clases para un gimnasio utilizando Next.js, AWS Amplify y DynamoDB. La plataforma permite gestionar reservas y almacenar datos de usuarios y clases de manera eficiente.',
+      'aboutMe1':'¡Hola! Soy Jonathan, un apasionado desarrollador de software con un enfoque en crear soluciones que hagan la vida de las personas más sencilla. Mi interés por la tecnología comenzó desde joven, cuando empecé a desarrollar pequeñas aplicaciones en la consola. Desde entonces, mi curiosidad no ha dejado de crecer, lo que me llevó a estudiar Ingeniería de Software y a buscar constantemente nuevos retos para mejorar mis habilidades.',
+      'aboutMe2':'A lo largo de mi camino, me he especializado en tecnologías como Next.js y React, y me entusiasma poder crear aplicaciones con estas herramientas que ofrecen gran flexibilidad y rendimiento. Me gusta profundizar en estas tecnologías y aplicar buenas prácticas para construir soluciones robustas y escalables.',
+      'aboutMe3':'Mi enfoque principal es siempre mejorar la experiencia del usuario y desarrollar software que sea fácil de usar y accesible. Además, disfruto del trabajo en equipo y de colaborar para resolver problemas complejos de manera eficiente y creativa.',
+
     },
     es: {
-        'saludo':'Hola, mi nombre es Jonathan'
+      'saludo':'Hola, mi nombre es Jonathan',
+      'nombre':'Jonathan Herrera',
+      'profecion':"Full Stack Developer",
+      'descripcion':"Hi, I'm Jonathan Herrera, a web developer and designer. I'm passionate about creating beautiful, accessible, and high-performance websites. I'm also a big fan of Astro, the fastest way to build modern websites.",
+      'trabajo':"Know a little more about my work",
+      'inicio':"Home",
+      'sobreMi':"About me",
+      'proyectos':"Projects",
+      'contacto':"Contact",
+      'ProyectName1':'Pet project',
+      'ProyectName2':'RentRide',
+      'ProyectName3':'FitNation',
+      'proyectDescription1':'I developed a booking platform using HTML, CSS, JavaScript, and SQL databases, designed to connect users looking for services for their pets with providers offering such services. The application allowed managing bookings efficiently and improved the experience for both customers and providers.',
+      'proyectDescription2':'I developed a vehicle rental platform with .NET and Azure, implementing reservation and user management, and using SQL to store information. We worked under SCRUM, overcoming cloud integration challenges to deliver a scalable solution.',
+      'proyectDescription3':'I developed a class booking page for a gym using Next.js, AWS Amplify, and DynamoDB. The platform allows managing bookings and storing user and class data efficiently.',
+      'aboutMe1':'Hello! I am Jonathan, a passionate software developer with a focus on creating solutions that make people\'s lives easier. My interest in technology began at a young age when I started developing small applications on the console. Since then, my curiosity has only grown, leading me to study Software Engineering and constantly seek new challenges to improve my skills.',
+      'aboutMe2':'Along the way, I have specialized in technologies like Next.js and React, and I am excited to create applications with these tools that offer great flexibility and performance. I like to delve into these technologies and apply best practices to build robust and scalable solutions.',
+      'aboutMe3':'My main focus is always to improve the user experience and develop software that is easy to use and accessible. In addition, I enjoy teamwork and collaborating to solve complex problems efficiently and creativel',
     },
   } as const;
